90290c82714234a6e465f2013ddd3e420e171172
William Ronchetti <wrr33@cornell.edu>
2019-10-03 10:23:18 -0400

    re-enabling logging test (again)

--- src/snovault/tests/test_indexing.py
+++ src/snovault/tests/test_indexing.py
@@ -294,7 +294,6 @@ def test_indexing_simple(app, testapp, indexer_testapp):
     # ensure we only have 1 shard for tests
     assert testing_ppp_settings['settings']['index']['number_of_shards'] == '1'
 
-@pytest.mark.skip # Log on travis is truncated, passes locally
 @pytest.mark.flaky(max_runs=3)
 def test_indexing_logging(app, testapp, indexer_testapp, capfd):
     """
