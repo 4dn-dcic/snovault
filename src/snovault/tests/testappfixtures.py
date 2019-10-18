fb9cad4fda77b4d4b220621ba8dcb9c729dab769
William Ronchetti <wrr33@cornell.edu>
2019-10-18 16:30:26 -0400

    enabling mpindexer

--- src/snovault/tests/testappfixtures.py
+++ src/snovault/tests/testappfixtures.py
@@ -5,7 +5,7 @@ _app_settings = {
     'item_datastore': 'database',
     'load_test_only': True,
     'testing': True,
-    'mpindexer': False,
+    'mpindexer': True,
     'pyramid.debug_authorization': True,
     'postgresql.statement_timeout': 20,
     'retry.attempts': 3,
