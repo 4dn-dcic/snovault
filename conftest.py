8cb3dba02512885e20a41829a2c9ab73b0bd6ca0
Kent Pitman <kent_pitman@hms.harvard.edu>
2020-06-05 06:09:19 -0400

    Add some internal documentation of rationale.

--- conftest.py
+++ conftest.py
@@ -20,4 +20,7 @@ def aws_auth(request):
 
 
 def pytest_configure():
+    # This adjustment is important to set the default choice of temporary filenames to a nice short name
+    # because without it some of the filenames we generate end up being too long, and critical functionality
+    # ends up failing. Some socket-related filenames, for example, seem to have length limits. -kmp 5-Jun-2020
     tempfile.tempdir = '/tmp'
