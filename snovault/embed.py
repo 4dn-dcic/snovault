b10049eaa514bb592cfa827cb7e13afd4900159d
William Ronchetti <wrr33@cornell.edu>
2020-10-09 14:27:34 -0400

    space

--- snovault/embed.py
+++ snovault/embed.py
@@ -116,7 +116,7 @@ def embed(request, *elements, **kw):
 
     # NOTE: if result was retrieved from ES, the following cached attrs will be
     # empty: _aggregated_items, _linked_uuids, _rev_linked_by_item
-    #result = deepcopy(cached['result'])
+    # result = deepcopy(cached['result'])
 
     # aggregated_items may be cached; if so, add them to the request
     # these conditions only fulfilled when using @@embedded and aggregated
