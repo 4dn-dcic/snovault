ea5e3f1f674e28919f9750583a91e658ad81e63f
William Ronchetti <wrr33@cornell.edu>
2022-10-06 13:47:57 -0400

    disable 3.7 tests since we arent using 3.7 anymore

--- snovault/tests/test_storage.py
+++ snovault/tests/test_storage.py
@@ -281,9 +281,6 @@ def _test_S3BlobStorage(s3_encrypt_key_id, kms_args_expected):
         assert 'key' in download_meta
         mocked_s3_put_object.assert_called_once()
         call_kwargs = mocked_s3_put_object.call_args.kwargs
-        print(mocked_s3_put_object)
-        print(mocked_s3_put_object.call_args)
-        print(call_kwargs)
         if kms_args_expected:
             assert call_kwargs.get("ServerSideEncryption") == "aws:kms"
             assert call_kwargs.get("SSEKMSKeyId") == s3_encrypt_key_id
