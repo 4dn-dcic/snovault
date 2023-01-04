87054f5dd9c96897943ea944e52f62f6dc6090db
William Ronchetti <wrr33@cornell.edu>
2023-01-04 11:36:31 -0500

    cast to list

--- snovault/elasticsearch/create_mapping.py
+++ snovault/elasticsearch/create_mapping.py
@@ -1380,7 +1380,7 @@ def reindex_by_type_staggered(app):
         uuids = {}
         build_index(app, es, namespaced_index, i_type, mapping, uuids, False)
         mapping_end = timer()
-        indexer_queue.add_uuids(app.registry, uuids.values(), strict=True,
+        indexer_queue.add_uuids(app.registry, list(uuids.values()), strict=True,
                                 target_queue='secondary')
         log.warning(f'Queued type {i_type} in {mapping_end - current_start}')
         time.sleep(5)
