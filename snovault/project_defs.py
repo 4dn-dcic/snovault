736b6aab83616a997b4904defb6e01b2643ae09d
Kent Pitman <kent_pitman@hms.harvard.edu>
2023-05-19 05:47:25 -0400

    Accept dcicutils 7.4.2.3b14 (kmp_project_utils_simple).

--- snovault/project_defs.py
+++ snovault/project_defs.py
@@ -1,9 +1,8 @@
-from dcicutils.project_utils import ProjectRegistry, C4Project
+from dcicutils.project_utils import C4ProjectRegistry, C4Project
 
-@ProjectRegistry.register('dcicsnovault')
+@C4ProjectRegistry.register('dcicsnovault')
 class SnovaultProject(C4Project):
-    NAME = 'snovault'
-    PYPI_NAME = 'dcicsnovault'
+    IDENTITY = {'NAME': 'snovault', 'PYPI_NAME': 'dcicsnovault'}
     ACCESSION_PREFIX = 'SNO'
 
 
