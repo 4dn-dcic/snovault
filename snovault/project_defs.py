03eef59ab16beb3432116340c8d2b86c02a67d52
David Michaels <david_michaels@hms.harvard.edu>
2023-07-21 14:11:09 -0400

    Moved test_renderers.py here from cgap-portal/fourfront.

--- snovault/project_defs.py
+++ snovault/project_defs.py
@@ -3,6 +3,7 @@ from .project.access_key import SnovaultProjectAccessKey
 from .project.authentication import SnovaultProjectAuthentication
 from .project.authorization import SnovaultProjectAuthorization
 from .project.ingestion import SnovaultProjectIngestion
+from .project.loadxl import SnovaultProjectLoadxl
 
 
 @C4ProjectRegistry.register("dcicsnovault")
@@ -10,7 +11,7 @@ class SnovaultProject(SnovaultProjectAccessKey,
                       SnovaultProjectAuthentication,
                       SnovaultProjectAuthorization,
                       SnovaultProjectIngestion,
-                      
+                      SnovaultProjectLoadxl,
                       C4Project):
     NAMES = {"NAME": "snovault", "PYPI_NAME": "dcicsnovault"}
     ACCESSION_PREFIX = "SNO"
